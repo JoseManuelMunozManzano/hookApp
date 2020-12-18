398100664a018bb50bba81b572d3474cccae0854
Neimerc <Neil_Mercury@yahoo.es>
2020-12-18 15:31:50 +0100

    useEffect - Precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,20 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
-  useEffect(() => {
-    console.log('componente montado');
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
 
-    // Qué es lo que retorna un useEffect?
-    // Retorna una función que se va a disparar cuando el componente se desmonta o, dicho de
-    // otro modo, deja de existir. Es una función que se ejecuta para hacer una limpieza.
-    return () => {
-      console.log('componente desmontado');
+  const { x, y } = coords;
+
+  useEffect(() => {
+    // Imaginemos que queremos extraer las coordenadas de la pantalla cuando este
+    // componente este abierto.
+    const mouseMove = e => {
+      const coords = { x: e.x, y: e.y };
+      setCoords(coords);
     };
-  }, []);
+
+    window.addEventListener('mousemove', mouseMove);
+
+    // Hay que tener mucho cuidado con esto. Cada vez que se accede al efecto se está
+    // creando un nuevo listener que hace exactamente lo mismo y esto consume muchísima
+    // memoria.
+    // Hay que hacer un procedimiento de limpieza cuando el componente ya no se use y se
+    // necesite eliminar el listener.
+    return () => {
+      window.removeEventListener('mousemove', mouseMove);
+    };
+  }, [coords]);
 
   return (
     <div>
       <h3>Eres genial</h3>
+      <p>
+        x: {x} y: {y}
+      </p>
     </div>
   );
 };
