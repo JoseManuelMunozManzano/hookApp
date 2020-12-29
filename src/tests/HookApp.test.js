dfa8418c01d801b4fc0f7e4dd8f4a715b8bbcd4e
Neimerc <Neil_Mercury@yahoo.es>
2020-12-29 16:30:09 +0100

    tests - Inicio de Pruebas sobre Hooks

---
+++ src/tests/HookApp.test.js
@@ -0,0 +1,9 @@
+import { shallow } from 'enzyme';
+import HookApp from '../HookApp';
+
+describe('Pruebas en <HookApp />', () => {
+  test('debe de mostrarse correctamente', () => {
+    const wrapper = shallow(<HookApp />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
