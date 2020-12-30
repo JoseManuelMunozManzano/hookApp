3b140fe0d8d0568c4c17fbb34b875ed80c4eb6bf
Neimerc <Neil_Mercury@yahoo.es>
2020-12-30 07:42:43 +0100

    Pruebas sobre el Reducer

---
+++ src/tests/fixtures/demoTodos.js
@@ -0,0 +1,13 @@
+// La carpeta fixtures contiene ficheros con datos que se usarán en las distintas pruebas.
+export const demoTodos = [
+  {
+    id: 1,
+    desc: 'Aprender React',
+    done: false,
+  },
+  {
+    id: 2,
+    desc: 'Aprender Mongo',
+    done: false,
+  },
+];
